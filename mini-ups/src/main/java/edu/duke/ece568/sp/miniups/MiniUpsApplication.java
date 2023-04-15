c3e9be011542cf1f1693ab3693f6f83d0402d6c0
Xu (Jordan) Han <xh123@duke.edu>
2023-04-15 12:03:25 -0400

    init commit, empty spring-boot app

---
+++ mini-ups/src/main/java/edu/duke/ece568/sp/miniups/MiniUpsApplication.java
@@ -0,0 +1,13 @@
+package edu.duke.ece568.sp.miniups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniUpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniUpsApplication.class, args);
+	}
+
+}
---
+++ mini-ups/src/test/java/edu/duke/ece568/sp/miniups/MiniUpsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.duke.ece568.sp.miniups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniUpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
