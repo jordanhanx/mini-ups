de4a6d32923e5da2ff6da38959e00e505ea0cc35
Xu (Jordan) Han <xh123@duke.edu>
2023-04-21 21:05:18 -0400

    connect to world successfully

--- mini-ups/src/main/java/edu/duke/ece568/team24/miniups/connection/ProtoSender.java
+++ mini-ups/src/main/java/edu/duke/ece568/team24/miniups/connection/ProtoSender.java
@@ -3,6 +3,7 @@ package edu.duke.ece568.team24.miniups.connection;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -20,18 +21,18 @@ public class ProtoSender implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // try (Socket socket = new Socket("localhost", 23456)) {
-        UInitTruck truck1 = UInitTruck.newBuilder().setId(1).setX(0).setY(0).build();
-        UInitTruck truck2 = UInitTruck.newBuilder().setId(2).setX(0).setY(0).build();
-        UConnect uconnect = UConnect.newBuilder()
-                .setIsAmazon(false)
-                .addAllTrucks(List.of(truck1, truck2))
-                .build();
-        ByteArrayOutputStream outBytes = new ByteArrayOutputStream();
-        uconnect.writeDelimitedTo(outBytes);
-        ByteArrayInputStream inBytes = new ByteArrayInputStream(outBytes.toByteArray());
-        UConnect uconnect2 = UConnect.parseDelimitedFrom(inBytes);
-        System.out.println(uconnect2.toString());
-        // }
+        try (Socket socket = new Socket("localhost", 12345)) {
+            UInitTruck truck1 = UInitTruck.newBuilder().setId(1).setX(0).setY(0).build();
+            UInitTruck truck2 = UInitTruck.newBuilder().setId(2).setX(0).setY(0).build();
+            UConnect uConnect = UConnect.newBuilder()
+                    .setIsAmazon(false)
+                    .addAllTrucks(List.of(truck1, truck2))
+                    .build();
+            OutputStream outputStream = socket.getOutputStream();
+            uConnect.writeDelimitedTo(outputStream);
+            InputStream inputStream = socket.getInputStream();
+            UConnected uConnected = UConnected.parseDelimitedFrom(inputStream);
+            System.out.println(uConnected.toString());
+        }
     }
 }
